f5509d7bd860016f55a3a8b722a665a7fb244816
0xtorchieG <gerald@blocktorch.xyz>
2024-03-15 22:10:21 +0000

    configuring spicy testnet chain

--- packages/app/src/utils/network.ts
+++ packages/app/src/utils/network.ts
@@ -1,16 +1,13 @@
-import { mainnet, sepolia, polygon, optimism, arbitrum, Chain, hardhat } from 'viem/chains'
+import {spicy, arbitrum, Chain } from 'viem/chains'
 
-let chains = [mainnet, sepolia, polygon, optimism, arbitrum] as [Chain, ...Chain[]]
+let chains = [spicy, arbitrum] as [Chain, ...Chain[]]
 
-if (process.env.NODE_ENV !== 'production') chains.push(hardhat)
 
 export const ETH_CHAINS = chains
 
 export function GetNetworkColor(chain?: string) {
-  if (chain === 'homestead') return 'green'
   if (chain === 'arbitrum') return 'blue'
-  if (chain === 'optimism') return 'red'
-  if (chain === 'matic') return 'purple'
+  if (chain === 'spicy') return 'red'
 
   return 'grey'
 }
