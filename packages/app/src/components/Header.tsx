62348e7c94b1af4aa92f49de6d5c03f6900fbdef
0xtorchieG <gerald@blocktorch.xyz>
2024-03-15 22:41:06 +0000

    adding initial app structure and navigation

--- packages/app/src/components/Header.tsx
+++ packages/app/src/components/Header.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import { LinkComponent } from './LinkComponent'
-import { SITE_NAME } from '@/utils/site'
 import { Connect } from './Connect'
 
 export function Header() {
   return (
-    <header className='navbar flex justify-between p-4 pt-0'>
-      <LinkComponent href='/'>
-        <h1 className='text-lg font-bold'>{SITE_NAME}</h1>
-      </LinkComponent>
-
+    <header className='navbar sticky flex bg-neutral justify-end h-10 p-4'>
       <Connect />
     </header>
   )
--- packages/app/src/components/Layout.tsx
+++ packages/app/src/components/Layout.tsx
@@ -1,13 +1,51 @@
 import React, { PropsWithChildren } from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
+import Link from 'next/link'
+import SportsSoccerOutlinedIcon from '@mui/icons-material/SportsSoccerOutlined'
+import WorkspacePremiumOutlinedIcon from '@mui/icons-material/WorkspacePremiumOutlined'
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined'
 
 export function Layout(props: PropsWithChildren) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Header />
+    <div className='flex flex-col min-h-screen bg-base-200'>
+      <div className='drawer lg:drawer-open z-30'>
+        <input id='my-drawer-2' type='checkbox' className='drawer-toggle' />
+        <div className='drawer-content flex flex-col items-center justify-center'>
+          <Header />
+          <main className='flex-grow container mx-auto ml-0'>{props.children}</main>
+          <label htmlFor='my-drawer-2' className='btn btn-primary drawer-button lg:hidden'>
+            Open drawer
+          </label>
+        </div>
+        <div className='drawer-side bg-neutral'>
+          <label htmlFor='my-drawer-2' aria-label='close sidebar' className='drawer-overlay'></label>
+          <Link href='/'>
+            <h1 className='text-xl font-bold pl-6 pt-4 h-16 text-white'>SpiceUp</h1>
+          </Link>
 
-      <main className='flex-grow px-4 container max-w-3xl mx-auto'>{props.children}</main>
+          <ul className='menu p-4 w-56 min-h-full bg-base-300 text-base-content'>
+            <li className='mt-4 mb-4'>
+              <Link href='/challenges'>
+                <WorkspacePremiumOutlinedIcon />
+                <p>Challenges</p>
+              </Link>
+            </li>
+            <li className='mb-4'>
+              <Link href='/user-profile'>
+                <AccountBoxOutlinedIcon />
+                <p>Your profile</p>
+              </Link>
+            </li>
+            <li>
+              <Link href='/clubs-area'>
+                <SportsSoccerOutlinedIcon />
+                <p>Clubs area</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Footer />
     </div>
   )
